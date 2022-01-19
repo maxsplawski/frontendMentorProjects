e165d053defc280ae983b4208aa3d0912e1e75af
Max <mniamek9@gmail.com>
2022-01-19 18:30:49 +0100

    Development
    Development of the mobile menu

--- easybankWebsite/app/js/main.js
+++ easybankWebsite/app/js/main.js
@@ -1,9 +1,23 @@
-const btnMenu = document.querySelector(".header__menu");
+const btnMenu = document.querySelector(".header__toggle");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const menu = document.querySelector(".header__menu");
 
 btnMenu.addEventListener("click", function () {
-  if (btnMenu.classList.contains("open")) {
-    btnMenu.classList.remove("open");
+  if (header.classList.contains("open")) {
+    body.classList.add("noscroll");
+    header.classList.remove("open");
+    menu.classList.add("fade-out");
+    menu.classList.remove("fade-in");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
   } else {
-    btnMenu.classList.add("open");
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    menu.classList.remove("fade-out");
+    menu.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
   }
 });
